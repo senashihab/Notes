67897cd69fdce416e6a842cebc4ac25681ee6ace
unknown <you@example.com>
2026-05-12 11:43:33 +0530

    updated code

--- bluewhale/urls.py
+++ bluewhale/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('delete/<int:id>/', views.delete, name='delete'),
+    path('update/<int:id>/',views.update, name='update'),
 ]
 
--- bluewhale/views.py
+++ bluewhale/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from .models import Note
 
+
+# HOME PAGE
+
 def index(request):
 
     tasks = Note.objects.all()
@@ -21,6 +24,8 @@ def index(request):
     return render(request, 'index.html', {'tasks':tasks})
 
 
+# DELETE
+
 def delete(request, id):
 
     note = Note.objects.get(id=id)
@@ -28,3 +33,27 @@ def delete(request, id):
     note.delete()
 
     return redirect('/')
+
+
+# UPDATE
+
+def update(request, id):
+
+    note = Note.objects.get(id=id)
+
+    tasks = Note.objects.all()
+
+    if request.method == 'POST':
+
+        note.title = request.POST.get('title')
+
+        note.content = request.POST.get('content')
+
+        note.save()
+
+        return redirect('/')
+
+    return render(request, 'index.html', {
+        'edit_note':note,
+        'tasks':tasks
+    })
